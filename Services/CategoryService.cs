861c6921cb830148b5d5f409a5d913e3581bf305
SamuelRBatista <samuel.r.batista@outlook.com>
2024-10-31 12:22:25 -0300

    feat:31102024 => Implementação do services

---
+++ Services/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿using SrbComercialAdmin.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+public class CategoryService
+{
+    private readonly HttpClient _httpClient;
+
+    public CategoryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Category>> GetCategoriesAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Category>>("https://localhost:7056/api/Category");
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using SrbComercialAdmin.Models;
+
+namespace SrbComercialAdmin.Services;
+
+public class ProductService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Product>> GetProductAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Product>>("https://localhost:7056/api/Product");
+    }
+
+    public async Task CreateProductAsync(Product product)
+    {
+        var response = await _httpClient.PostAsJsonAsync("https://localhost:7056/api/Product", product);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"https://localhost:7056/api/Product/{product.Id}", product);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task DeleteProductAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"https://localhost:7056/api/Product/{id}");
+        response.EnsureSuccessStatusCode();
+    }
+
+
+
+
+
+}
