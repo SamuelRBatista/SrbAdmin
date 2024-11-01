cea25c581f383e6ead6da3129e7fc3dc4a843a95
SamuelRBatista <samuel.r.batista@outlook.com>
2024-11-01 14:04:25 -0300

    refactor:01112024 => Finalizando a implentação do restante do metodos de crud

---
+++ Services/SupplierService.cs
@@ -0,0 +1,38 @@
+﻿using SrbComercialAdmin.Models;
+
+namespace SrbComercialAdmin.Services;
+
+
+public class SupplierService
+{
+
+    private readonly HttpClient _httpClient;
+
+    public SupplierService(HttpClient httpClient)
+    {
+            _httpClient = httpClient;
+    }
+
+    public async Task<List<Supplier>> GetSupplierAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Supplier>>("https://localhost:7056/api/Supplier");
+    }
+
+    public async Task CreateSupplierAsync(Supplier supplier)
+    {
+        var response = await _httpClient.PostAsJsonAsync($"https://localhost:7056/api/Supplier", supplier);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task UpdateSupplierAsync(Supplier supplier)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"https://localhost:7056/api/Supplier/{supplier.Id}", supplier);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task DeleteSupplierAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"https://localhost:7056/api/Supplier/{id}");
+        response.EnsureSuccessStatusCode();
+    }
+}
