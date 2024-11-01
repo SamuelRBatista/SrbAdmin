014bd4efa56190c449e1554ff27e5884ccea863e
SamuelRBatista <samuel.r.batista@outlook.com>
2024-11-01 14:03:30 -0300

    refactor:01112024 => Ajuste na model de Cliente e Fornecedor

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -1,14 +1,16 @@
 ﻿namespace SrbComercialAdmin.Models;
 
-public class Client
+public class Supplier
 {
     public int Id { get; set; }
-    public string? Cpf { get; set; }
+    public string? Cnpj { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
+    public string? Phone { get; set; }
     public string? Cep { get; set; }
     public string? Address { get; set; }
-    public int CityId { get; set; }
-    public string? CityName { get; set; }
-    public int StateId { get; set; }
+    public int CityId { get; set; } 
+
+    public int StateId { get; set; }    
+   
 }
